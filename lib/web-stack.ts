d9039dfdf404fa4b97016afa0a6742b97d98d817
pm-muraoka <shotaro_muraoka@pressman.ne.jp>
2020-09-14 23:27:36 +0900

    mod typo

--- lib/web-stack.ts
+++ lib/web-stack.ts
@@ -42,7 +42,7 @@ export default class WebStack extends Stack {
             'systemctl enable docker',
             'systemctl start docker',
             'curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
-            'chomd +x /usr/local/bin/docker-compose',
+            'chmod +x /usr/local/bin/docker-compose',
             'wget https://aws-codedeploy-ap-northeast-1.s3.ap-northeast-1.amazonaws.com/latest/install',
             'chmod +x ./install',
             './install auto',
